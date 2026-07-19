d0b9988ee41cf38f64ccb2b46adfe8a243e27f9e
Peter H. Boling <peter.boling@gmail.com>
2026-07-19 00:49:43 -0600

    fix(elixir): generate typed e2e configs

--- test_apps/elixir/mix.exs
+++ test_apps/elixir/mix.exs
@@ -3,18 +3,18 @@ defmodule E2eElixir.MixProject do
 
   def project do
     [
-    app: :e2e_elixir,
-    version: "0.1.0",
-    elixir: "~> 1.14",
-    deps: deps()
+      app: :e2e_elixir,
+      version: "0.1.0",
+      elixir: "~> 1.14",
+      deps: deps()
     ]
   end
 
   defp deps do
     [
-    {:tree_sitter_language_pack, "1.12.5"},
-    {:rustler_precompiled, "~> 0.9"},
-    {:rustler, "~> 0.37", runtime: false}
+      {:tree_sitter_language_pack, "1.12.5"},
+      {:rustler_precompiled, "~> 0.9"},
+      {:rustler, "~> 0.37", runtime: false}
     ]
   end
 end
