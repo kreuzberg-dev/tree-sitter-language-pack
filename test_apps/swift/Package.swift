ca0e1a3dd30fdf9f47f98bb90a401828d33541b7
Na'aman Hirschfeld <nhirschfeld@gmail.com>
2026-08-19 20:31:11 +0200

    chore(release): 1.15.2

--- test_apps/swift/Package.swift
+++ test_apps/swift/Package.swift
@@ -8,7 +8,7 @@ let package = Package(
     .iOS(.v14),
   ],
   dependencies: [
-    .package(url: "https://github.com/xberg-io/tree-sitter-language-pack.git", from: "1.15.1"),
+    .package(url: "https://github.com/xberg-io/tree-sitter-language-pack.git", from: "1.15.2"),
   ],
   targets: [
     .testTarget(
